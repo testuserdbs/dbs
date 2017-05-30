ae521e8e5f37dbf5a179d345cd1ec24eaf8bb647
testuserdbs <bigcel@hotmail.de>
2017-05-30 14:39:22 +0200

    neues update

--- tweets.sql
+++ tweets.sql
@@ -1,3 +1,6 @@
-CREATE TABLE tweet(tweet_id varchar NOT NULL, favorite_count int, retweet_count int, t_time int, text varchar, CONSTRAINT tweet_pk PRIMARY KEY (tweet_id) );
+CREATE TABLE tweet(tweet_id int NOT NULL, favorite_count int, retweet_count int, t_time int, text varchar, CONSTRAINT tweet_pk PRIMARY KEY (tweet_id) );
 CREATE TABLE hashtag(h_time int, h_name varchar NOT NULL, CONSTRAINT hashtag_pk PRIMARY KEY (h_name));
-
+CREATE TABLE hat_einen(tweet_id int,h_name varchar, CONSTRAINT hat_einen_pk PRIMARY KEY (tweet_id, h_name));
+ALTER TABLE hat_einen ADD CONSTRAINT hat_einen_fk FOREIGN KEY (h_name) REFERENCES hashtag (h_name);
+ALTER TABLE hat_einen ADD CONSTRAINT hat_einen_fk FOREIGN KEY (tweet_id) REFERENCES tweet (tweet_id);
+# dbs
